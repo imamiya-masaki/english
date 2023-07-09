28cea1777ab131e4806815c7b923912d1d714fea
imamiya-masaki <imamiyagit@gmail.com>
2023-07-10 07:44:53 +0900

    box 設置

---
+++ src/app/translator/page.tsx
@@ -0,0 +1,41 @@
+import { css } from '../../../styled-system/css';
+ 
+
+type SystemStyleObject = Parameters<typeof css>[0];
+
+export default function Home() {
+
+  const topCss: SystemStyleObject = {
+    display: "flex",
+    margin: "0 autho;",
+    paddingLeft: "100px",
+    paddingRight: "100px",
+    columnGap: "1%"
+  }
+
+  const boxCss: SystemStyleObject = {
+    "minHeight": "clamp(150px, 250px, 500px)",
+    "width": "calc(50% - 16px)",
+  }
+
+  const textareaCss: SystemStyleObject = {
+    "width": "100%",
+    "height": "100%",
+    "backgroundColor": "#eeeeee",
+  }
+
+  return (
+    <div className={css(topCss)}>
+      <div className={css(boxCss)} id="left">
+        <textarea className={css(textareaCss)}>
+          
+        </textarea>
+      </div>
+      <div className={css(boxCss)} id="right">
+        <textarea className={css(textareaCss)}>
+          
+        </textarea>
+      </div>
+    </div>
+  )
+}
